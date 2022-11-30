7e1c8a3290b6516a305cb565b2eb7ab8b1befe29
Ilham Ramadhan <ilham.ramadhan.permadi@gmail.com>
2022-11-30 09:54:30 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://ilham:93814@cluster0.i8lyesr.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
@@ -18,7 +31,7 @@ def homework_post():
         'comment': comment_receive,
     }
     db.fanmessages.insert_one(doc)
-    return jsonify({'msg':'POST request!'})
+    return jsonify({'msg':'Comment Posted!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
@@ -26,4 +39,4 @@ def homework_get():
     return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
